e80ed7f80e1e9949b87660f73dc5cdd3a72aecb6
wintheradam123 <wintheradam123@gmail.com>
2025-01-26 14:39:17 +0100

    init

---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace PentiaDemoMVC.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+        public int OrderPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int SalespersonId { get; set; }
+    }
+}
---
+++ Models/Salesperson.cs
@@ -0,0 +1,12 @@
+﻿namespace PentiaDemoMVC.Models
+{
+    public class Salesperson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
